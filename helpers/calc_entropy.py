d39200b8da8a7b0e006aada86b039c4b87770fdb
Eugene Polevikov <eugene.polevikov@yandex.ru>
2025-12-30 17:33:07 +0300

    Add script to calc entropy

---
+++ helpers/calc_entropy.py
@@ -0,0 +1,16 @@
+import sys
+from collections import Counter
+import math
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "rb") as f:
+        input_bytes = f.read()
+
+    bytes_by_counts = Counter(input_bytes).most_common()
+    entropy = 0.0
+
+    for b, count in bytes_by_counts: 
+        entropy += -(count / len(input_bytes)) * math.log(count / len(input_bytes))
+    
+    print(round(entropy, 3), round(8 / entropy, 3))
