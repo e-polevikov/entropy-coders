d5192b086f0f2d68a9d0125d7b4601d1b50362fd
Eugene Polevikov <eugene.polevikov@yandex.ru>
2025-12-30 16:59:33 +0300

    Add script to generate squeezed distribution

---
+++ helpers/generate_squeezed.py
@@ -0,0 +1,22 @@
+import random
+import array
+
+def main():
+    N = 10**5
+    squeezed = []
+
+    for _ in range(N):
+        x = random.randint(1, 100)
+
+        if 1 <= x < 80:
+            squeezed.append(0)
+        elif 80 <= x < 90:
+            squeezed.append(1)
+        else:
+            squeezed.append(2)
+
+    with open("squeezed.bin", "wb") as f:
+        f.write(bytes(squeezed))
+
+if __name__ == "__main__":
+    main()
