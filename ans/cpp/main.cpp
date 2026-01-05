239ecd237a9ee358b023c4434b0ea5323e1f246a
Eugene Polevikov <eugene.polevikov@yandex.ru>
2026-01-05 15:13:57 +0300

    Implement rANS compressor (WIP)

--- ans/cpp/main.cpp
+++ ans/cpp/main.cpp
@@ -10,15 +10,18 @@ int main(int argc, char* argv[]) {
     }
 
     std::uintmax_t filesize = std::filesystem::file_size(argv[1]);
+
     uint8_t *buffer = new uint8_t[filesize];
+    uint8_t *dst = new uint8_t[filesize];
 
     std::ifstream file(argv[1], std::ios::binary);
     file.read((char*) buffer, filesize);
 
-    rANS::compress(buffer, filesize, nullptr);
+    rANS::compress(buffer, filesize, dst);
     //rANS::decompress(nullptr, nullptr);
 
     delete [] buffer;
+    delete [] dst;
 
     return 0;
 }
--- ans/cpp/rans.cpp
+++ ans/cpp/rans.cpp
@@ -22,6 +22,26 @@ void compress(uint8_t* src, uint64_t src_size, uint8_t* dst) {
     }
 
     freq[max_i] += static_cast<int64_t>(1 << 16) - sum;
+
+    uint64_t cumul[1 + 256] = {};
+    for (uint64_t i = 0; i < 256; i++) {
+        cumul[i + 1] = cumul[i] + freq[i];
+    }
+
+    uint32_t* block = reinterpret_cast<uint32_t*>(dst);
+    uint64_t  state = 1 << 16;
+
+    for (uint64_t i = 0; i < src_size; i++) {
+        uint8_t symbol = src[i];
+
+        if (state >= freq[symbol] << 32) {
+            *block = state & 0xffffffff;
+            state >>= 32;
+            block++;
+        }
+
+        state = cumul[symbol] + state % freq[symbol] + ((state / freq[symbol]) << 16);
+    }
 }
 
 void decompress(uint8_t* src, uint8_t* dst) {
