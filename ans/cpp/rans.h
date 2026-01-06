eba6385f6d9b4d7bda4397f39de6f1350e57bbb8
Eugene Polevikov <eugene.polevikov@yandex.ru>
2026-01-06 16:39:30 +0300

    Refactoring

--- ans/cpp/rans.h
+++ ans/cpp/rans.h
@@ -6,6 +6,10 @@
 namespace rANS
 {
 
+const static uint8_t  LOG2_L = 16;
+const static uint64_t L = 1 << LOG2_L;
+const static uint8_t  b = 32;
+
 inline static uint64_t compress(uint8_t* src, uint64_t src_size, uint8_t* dst) {
     uint64_t freq[256] = {};
 
@@ -15,7 +19,7 @@ inline static uint64_t compress(uint8_t* src, uint64_t src_size, uint8_t* dst) {
     uint64_t sum = 0;
 
     for (uint64_t i = 0; i < 256; i++) {
-        uint64_t f = static_cast<float>(freq[i]) / src_size * (1 << 16);
+        uint64_t f = static_cast<float>(freq[i]) / src_size * L;
 
         if (f == 0 && freq[i] > 0) { f = 1; }
         if (f > freq[max_i]) { max_i = i; }
@@ -24,7 +28,7 @@ inline static uint64_t compress(uint8_t* src, uint64_t src_size, uint8_t* dst) {
         sum += f;
     }
 
-    freq[max_i] += static_cast<int64_t>(1 << 16) - sum;
+    freq[max_i] += L - sum;
 
     uint64_t cumul[1 + 256] = {};
     for (uint64_t i = 0; i < 256; i++) {
@@ -32,17 +36,17 @@ inline static uint64_t compress(uint8_t* src, uint64_t src_size, uint8_t* dst) {
     }
 
     uint32_t* block = reinterpret_cast<uint32_t*>(dst + sizeof(uint64_t));
-    uint64_t  state = 1 << 16;
+    uint64_t  state = L;
 
     for (uint64_t i = 0; i < src_size; i++) {
         uint8_t symbol = src[i];
 
-        if (state >= freq[symbol] << 32) {
-            *block++ = state & 0xffffffff;
-            state >>= 32;
+        if (state >= freq[symbol] << b) {
+            *block++ = state & ((1ULL << b) - 1);
+            state >>= b;
         }
 
-        state = cumul[symbol] + state % freq[symbol] + ((state / freq[symbol]) << 16);
+        state = cumul[symbol] + state % freq[symbol] + ((state / freq[symbol]) << LOG2_L);
     }
 
     *reinterpret_cast<uint64_t*>(block) = state;
@@ -56,7 +60,10 @@ inline static uint64_t compress(uint8_t* src, uint64_t src_size, uint8_t* dst) {
         freq_dst[i] = freq[i];
     }
 
-    uint64_t compressed_size = 4 * (block - reinterpret_cast<uint32_t*>(dst)) + 256 * sizeof(uint16_t);
+    uint64_t compressed_size =
+        sizeof(uint32_t) * (block - reinterpret_cast<uint32_t*>(dst))
+        + 256 * sizeof(uint16_t);
+    
     *reinterpret_cast<uint64_t*>(dst) = compressed_size;
 
     return compressed_size;
